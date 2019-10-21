96cc4fea4fe8c5d66f3d665dd267eb1c9f2357f7
Yameen Ajani <yameenajani@gmail.com>
2019-10-21 10:58:49 +0530

    Initial commit

---
+++ accounts/api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from django.views.generic.base import RedirectView
+
+from tweets.api.views import (
+    TweetListAPIView,
+    )
+
+app_name = 'accounts'
+
+urlpatterns = [
+    url(r'^(?P<username>[\w.@+-]+)/tweet/$', TweetListAPIView.as_view(), name='list'), # /api/tweet/
+]
+
